fdfe75ec7c96c9dc64878b71d843ba71ab75e0c7
randrahim <rrahim3@gatech.edu>
2024-11-03 01:05:06 -0400

    Return the module

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
   $(".gallery").mauGallery({
-    columns: { xs: 1, sm: 2, md: 3, lg: 3, xl: 3 },
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
     lightBox: true,
     lightboxId: "myAwesomeLightbox",
     showTags: true,
